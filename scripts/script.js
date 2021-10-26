74d70ccaa72fcb19431c50821764c90fdb5fc55b
kypexfly <rczgraphics@live.com>
2021-10-25 22:18:00 -0500

    2nd commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// Initialise Carousel
+const newsCarousel = new Carousel(document.querySelector("#newsCarousel"), {
+    infinite: false,
+    Dots: false,
+    friction: 0.89,
+    slidesToSlide: 2,
+    fill: false,
+});
