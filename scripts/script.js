ed17a4d3ffdab5b9152ccb9bad846eb7d498e835
kypexfly <rczgraphics@live.com>
2022-03-28 09:26:28 -0500

    changes part 2

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,6 @@ const newsCarousel = new Carousel(document.querySelector("#newsCarousel"), {
     slidesToSlide: 2,
     fill: false,
 });
+
+ScrollReveal().reveal('.card-box', {reset:true});
+ScrollReveal().reveal('#newsCarousel', {reset:true});
