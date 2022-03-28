bfaa2f750a5316d99764179f49c7207e62124154
kypexfly <rczgraphics@live.com>
2022-03-28 14:39:56 -0500

    update 2.0

--- scripts/script.js
+++ scripts/script.js
@@ -7,5 +7,22 @@ const newsCarousel = new Carousel(document.querySelector("#newsCarousel"), {
     fill: false,
 });
 
-ScrollReveal().reveal('.card-box', {reset:true});
-ScrollReveal().reveal('#newsCarousel', {reset:true});
+$('#navig a').on('click', function(e) {
+    // prevent default anchor click behavior
+    e.preventDefault();
+
+    // store hash
+    var hash = this.hash;
+
+    if ($(hash).length) {
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 300, function() {
+            // Do something fun if you want!
+        });
+    }
+});
+
+ScrollReveal().reveal('#my-profile', {reset:true, duration:1000, distance: '50px'});
+ScrollReveal().reveal('.card-box', {reset:false, duration:1000, distance: '50px'});
+ScrollReveal().reveal('#newsCarousel', {reset:false, duration:1000});
